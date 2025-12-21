04d7dabda395d6ab36cde4e03dc1f9f28f475513
Fernando <fneirot@Subastopsac.onmicrosoft.com>
2025-12-21 09:51:34 -0300

    instalo storybook

---
+++ apps/storybook/.storybook/main.ts
@@ -0,0 +1,35 @@
+import type { StorybookConfig } from '@storybook/react-vite';
+import path from "path";
+import { dirname } from "path"
+
+import { fileURLToPath } from "url"
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+/**
+* This function is used to resolve the absolute path of a package.
+* It is needed in projects that use Yarn PnP or are set up within a monorepo.
+*/
+function getAbsolutePath(value: string): any {
+  return dirname(fileURLToPath(import.meta.resolve(`${value}/package.json`)))
+}
+const config: StorybookConfig = {
+  "stories": ["../../../packages/ui/src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  "addons": [
+    getAbsolutePath('@chromatic-com/storybook'),
+    getAbsolutePath('@storybook/addon-vitest'),
+    getAbsolutePath('@storybook/addon-a11y'),
+    getAbsolutePath('@storybook/addon-docs')
+  ],
+  "framework": getAbsolutePath('@storybook/react-vite'),
+  async viteFinal(config) {
+    if (config.resolve) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        "@tu-org/ui": path.resolve(__dirname, "../../../packages/ui/src"),
+      };
+    }
+    return config;
+  }
+};
+export default config;
